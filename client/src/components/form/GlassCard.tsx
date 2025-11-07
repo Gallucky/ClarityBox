2a495628bac4ff09fd5f3c8f797dfe9726632283
Gallucky <galbenabu1111@gmail.com>
2025-11-07 23:06:25 +0200

    Started work on the error/not found 404 page.

--- client/src/components/form/GlassCard.tsx
+++ client/src/components/form/GlassCard.tsx
@@ -2,13 +2,16 @@ type GlassCardProps = {
     children: React.ReactNode;
     className?: string;
     lightSource?: boolean;
+    centered?: boolean;
 };
 
 const GlassCard = (props: GlassCardProps) => {
-    const { children, className, lightSource = true } = props;
+    const { children, className, lightSource = true, centered = false } = props;
+
+    const centeredClasses = "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2";
 
     return (
-        <div className={`glass-card ${className}`}>
+        <div className={`glass-card ${className} ${centered ? centeredClasses : ""}`}>
             {children}
             {lightSource && <div className="light-source" />}
         </div>
--- client/src/pages/Error/Error.page.tsx
+++ client/src/pages/Error/Error.page.tsx
@@ -1,5 +1,46 @@
+import GlassCard from "@/components/form/GlassCard";
+import { TbError404 } from "react-icons/tb";
+import { BsFillHexagonFill } from "react-icons/bs";
+import BlurText from "@/components/text/BlurText";
+import { Button } from "@/components/ui/shadcn/button";
+import { useNavigate } from "react-router-dom";
+
 const Error = () => {
-    return <></>;
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <GlassCard
+                centered
+                className="md:grid! md:grid-cols-2 md:grid-rows-2 h-7/12! w-[90%]! place-items-center justify-center! gap-10">
+                <h2 className="text-3xl font-inter font-bold overflow-hidden row-start-1 row-end-1 col-start-2 col-end-2 text-outline text-destructive">
+                    404 Not Found
+                </h2>
+                <div className="relative size-[25dvh] md:row-span-2 md:col-start-1 md:col-end-1">
+                    <TbError404
+                        size={"80%"}
+                        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                        color="black"
+                    />
+                    <BsFillHexagonFill size={"100%"} className="size-full" color="red" />
+                </div>
+                <BlurText
+                    text="The page you are looking for does not exist."
+                    delay={150}
+                    animateBy="words"
+                    direction="top"
+                    className="overflow-hidden"
+                    textClassName="text-[3.8dvw]! font-rubik! font-bold overflow-hidden"
+                />
+                <Button
+                    onClick={() => navigate("/")}
+                    variant={"outline"}
+                    className="size-fit p-2! fluid- font-inter">
+                    Return to home page
+                </Button>
+            </GlassCard>
+        </>
+    );
 };
 
 export default Error;
