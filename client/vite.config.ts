674b75941bd386630d60dd48a3c26259b9015585
Gallucky <galbenabu1111@gmail.com>
2025-12-03 18:41:00 +0200

    testing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,16 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { fileURLToPath } from "url";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-// Fix for __dirname in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), tsconfigPaths()],
+    plugins: [react(), tailwindcss()],
     server: {
         watch: {
             usePolling: true,
