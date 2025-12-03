7830fc84ce68a3c98f2366e867eac44f055535f4
Gallucky <galbenabu1111@gmail.com>
2025-12-03 21:59:41 +0200

    The CORS still blocks the frontend/client-side - trying to implement a custom checker logic using origin callback object/method with an helper

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -4,9 +4,20 @@ const app = express();
 
 const ALLOWED_ORIGINS = (process.env.ALLOWED_ORIGINS || "").split(",");
 
+const startingWithAllowedOrigin = (origin) =>
+    ALLOWED_ORIGINS.some((allowedOrigin) => origin.startsWith(allowedOrigin));
+
 app.use(
     cors({
-        origin: ALLOWED_ORIGINS,
+        origin: {
+            origin: (origin, callback) => {
+                if (startingWithAllowedOrigin(origin)) {
+                    return callback(null, true);
+                } else {
+                    return callback(new Error("Not allowed by CORS"));
+                }
+            },
+        },
         credentials: true,
         optionsSuccessStatus: 200,
     })
