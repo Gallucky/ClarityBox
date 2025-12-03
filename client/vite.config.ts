83cd1b51c5ce31825af62fe95707afaf3971f193
Gallucky <galbenabu1111@gmail.com>
2025-12-03 18:52:05 +0200

    revert back

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,22 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: process.env.VITE_BASE_PATH || "/clarity-box",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@app": path.resolve(__dirname, "./src/app"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@errors": path.resolve(__dirname, "./src/errors"),
+            "@hooks": path.resolve(__dirname, "./src/hooks"),
+            "@lib": path.resolve(__dirname, "./src/lib"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@styles": path.resolve(__dirname, "./src/styles"),
+            "@types": path.resolve(__dirname, "./src/types"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+        },
+    },
     server: {
         watch: {
             usePolling: true,
