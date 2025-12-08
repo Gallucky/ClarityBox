76dbe8f3f9c4e0c847a24f284c2de407934a02c0
Gallucky <galbenabu1111@gmail.com>
2025-12-08 23:04:35 +0200

    QuickFix LoginResponse type conflict

--- client/src/types/LoginResponse.ts
+++ client/src/types/LoginResponse.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import type { User } from "./models/User";
-=======
-import type { User } from "@/types/models/User";
->>>>>>> eb4bbf67a3cc39e4af48a66c146f7a777a30bb37
 
 export type LoginResponse = {
     token: string;
